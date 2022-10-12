6839239b58f2c53862a0963391b87351d3461e1b
Rizzen Yazston <rizzen.yazston@gmail.com>
2022-10-12 21:14:02 +0200

    Added project's workspace Cargo.toml, and skeleton meta-crate 'i18n' in preparation of adding component crates.

---
+++ crates/i18n/src/lib.rs
@@ -0,0 +1,8 @@
+// This file is part of `i18n` crate. For the terms of use, please see the file
+// called LICENSE-BSD-3-Clause at the top level of the `i18n` crate.
+
+//! Copy README.asciidoc content to here before release.
+
+// Commented out for now until crates is added.
+//pub use pattern-rizzen-yazston as pattern;
+//pub use message-rizzen-yazston as message;
