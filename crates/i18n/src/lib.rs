cc518cbc1723b7bd2ceac9ef56253167993ec31e
Rizzen Yazston <rizzen.yazston@gmail.com>
2022-10-15 21:10:11 +0200

    Updated documentation to bump project to version 0.2.0.

--- crates/i18n/src/lib.rs
+++ crates/i18n/src/lib.rs
@@ -1,7 +1,28 @@
 // This file is part of `i18n` crate. For the terms of use, please see the file
 // called LICENSE-BSD-3-Clause at the top level of the `i18n` crate.
 
-//! Copy README.asciidoc content to here before release.
+//! `i18n` is the main meta-crate of the Internationalisation (`i18n`) project.
+//! 
+//! This convenience meta crate contains selected available crates:
+//! 
+//! * i18n_lstring-rizzen-yazston
+//! 
+//! # Usage
+//! 
+//! For most use cases, just the use of `i18n-rizzen-yazston` crate will be sufficient to use the multilingual message
+//! system, though the individual crates can be selected individual if the entire `i18n` project is not required.
+//! 
+//! ## Cargo.toml
+//! 
+//! ```
+//! [dependencies]
+//! icu_locid = "1.0.0"
+//! i18n-rizzen-yazston = "0.2.0"
+//! ```
+//! 
+//! ## Examples
+//! 
+//! See the various component crates for usage examples.
 
 pub use i18n_lstring-rizzen-yazston as lstring;
 //pub use pattern-rizzen-yazston as pattern;
