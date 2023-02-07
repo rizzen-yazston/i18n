3327f81b3b9aec95611232931418fddbc46db6c4
Rizzen Yazston <rizzen.yazston@gmail.com>
2023-02-07 08:08:08 +0200

    Started creating error struct.

---
+++ crates/error/src/lib.rs
@@ -0,0 +1,31 @@
+// This file is part of `i18n_error-rizzen-yazston` crate. For the terms of use, please see the file
+// called `LICENSE-BSD-3-Clause` at the top level of the `i18n_error-rizzen-yazston` crate.
+
+//! TODO
+//! 
+//! # Examples
+//! 
+//! ```
+//! //TODO
+//! ```
+//! 
+
+use i18n_lstring::LString;
+use std::rc::Rc;
+
+/// Error struct returned by functions encountering an error.
+
+pub struct Error {
+}
+
+/*
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check() {
+
+    }
+}
+*/
