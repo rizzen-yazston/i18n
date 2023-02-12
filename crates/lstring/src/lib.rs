e3baa2de02b5e443f3e563e6e1d9a2357781802f
Rizzen Yazston <rizzen.yazston@gmail.com>
2023-02-12 11:57:52 +0200

    Updated 'i18n_lstring' crate: change 'new()' to use 'Into<String>' instead of 'String'. Removed dependency on 'icu_locid' crate, by using 'Rc<String>' instead of 'Locale' for the language tag. Unit tests, examples and documentation updated.

--- crates/lstring/src/lib.rs
+++ crates/lstring/src/lib.rs
@@ -3,15 +3,10 @@
 
 //! Language string.
 //! 
-//! This crate contains the `LString` type (aka LanguageString), for associating a text string ([`String`]) to a
-//! specific language ([`Locale`]).
+//! This crate contains the `LString` type (aka LanguageString), for associating a text string (`[String]`) to a
+//! specific language (`[Rc]<String>`).
 //! 
-//! The [`Locale`] type can be provided by either the [`icu_locid`] crate or the [`icu`] meta-crate. These two crates
-//! are part of the [ICU4X] protect developed by the [Unicode Consortium].
-//! 
-//! This crate makes use of the [`Locale`] type instead of the [`LanguageIdentifier`] type due to that the [`Locale`]
-//! type supports the entire [BCP 47 Language Tag] specification, where as the [`LanguageIdentifier`] type excludes the
-//! **extension** subtags of the BCP 47 Language Tag specification.
+//! The specified language is expected to be a [BCP 47 Language Tag] string, though any identifier could be used.
 //! 
 //! # Examples
 //! 
@@ -21,36 +16,27 @@
 //! use i18n_lstring::LString;
 //! 
 //! let string = "This is a test string.";
-//! let locale = Rc::new( "en-ZA".parse().expect( "Failed to parse language tag." ) );
-//! let lang_string = LString::new( string, &locale );
+//! let tag = Rc::new(
+//!     Locale::canonicalize( "en-ZA" ).expect( "Failed to canonicalise language tag." )
+//! );
+//! let lang_string = LString::new( string, &tag );
 //! 
 //! assert_eq!( lang_string.as_str(), string, "String failed." );
-//! assert_eq!( lang_string.locale(), &locale, "Locale failed." );
+//! assert_eq!( lang_string.language_tag(), &tag, "Language tag failed." );
 //! ```
 //! 
-//! [`String`]: https://doc.rust-lang.org/std/string/struct.String.html
-//! [`Locale`]: https://docs.rs/icu/latest/icu/locid/struct.Locale.html
-//! [`icu_locid`]: https://crates.io/crates/icu_locid
-//! [`icu`]: https://crates.io/crates/icu
-//! [ICU4X]: https://github.com/unicode-org/icu4x
-//! [Unicode Consortium]: https://home.unicode.org/
-//! [`LanguageIdentifier`]: https://docs.rs/icu/latest/icu/locid/struct.LanguageIdentifier.html
+//! [String]: https://doc.rust-lang.org/std/string/struct.String.html
+//! [Rc]: https://doc.rust-lang.org/std/rc/struct.Rc.html
 //! [BCP 47 Language Tag]: https://www.rfc-editor.org/rfc/bcp/bcp47.txt
 
-use icu_locid::Locale;
 use std::rc::Rc;
 
 /// Language string.
 /// 
-/// This crate contains the `LString` type (aka LanguageString), for associating a text string ([`String`]) to a
-/// specific language ([`Locale`]).
+/// This crate contains the `LString` type (aka LanguageString), for associating a text string (`[String]`) to a
+/// specific language (`[Rc]<String>`).
 /// 
-/// The [`Locale`] type can be provided by either the [`icu_locid`] crate or the [`icu`] meta-crate. These two crates
-/// are part of the [ICU4X] protect developed by the [Unicode Consortium].
-/// 
-/// This crate makes use of the [`Locale`] type instead of the [`LanguageIdentifier`] type due to that the [`Locale`]
-/// type supports the entire [BCP 47 Language Tag] specification, where as the [`LanguageIdentifier`] type excludes the
-/// **extension** subtags of the BCP 47 Language Tag specification.
+/// The specified language is expected to be a [BCP 47 Language Tag] string, though any identifier could be used.
 /// 
 /// # Examples
 /// 
@@ -60,29 +46,38 @@ use std::rc::Rc;
 /// use i18n_lstring::LString;
 /// 
 /// let string = "This is a test string.";
-/// let locale = Rc::new( "en-ZA".parse().expect( "Failed to parse language tag." ) );
-/// let lang_string = LString::new( string, &locale );
+/// let tag = Rc::new(
+///     Locale::canonicalize( "en-ZA" ).expect( "Failed to canonicalise language tag." )
+/// );
+/// let lang_string = LString::new( string, &tag );
 /// 
 /// assert_eq!( lang_string.as_str(), string, "String failed." );
-/// assert_eq!( lang_string.locale(), &locale, "Locale failed." );
+/// assert_eq!( lang_string.language_tag(), &tag, "Language tag failed." );
 /// ```
 /// 
-/// [`String`]: https://doc.rust-lang.org/std/string/struct.String.html
-/// [`Locale`]: https://docs.rs/icu/latest/icu/locid/struct.Locale.html
-/// [`icu_locid`]: https://crates.io/crates/icu_locid
-/// [`icu`]: https://crates.io/crates/icu
-/// [ICU4X]: https://github.com/unicode-org/icu4x
-/// [Unicode Consortium]: https://home.unicode.org/
-/// [`LanguageIdentifier`]: https://docs.rs/icu/latest/icu/locid/struct.LanguageIdentifier.html
+/// [String]: https://doc.rust-lang.org/std/string/struct.String.html
+/// [Rc]: https://doc.rust-lang.org/std/rc/struct.Rc.html
 /// [BCP 47 Language Tag]: https://www.rfc-editor.org/rfc/bcp/bcp47.txt
 #[derive( PartialEq, Debug )]
 pub struct LString {
     string: String,
-    locale: Rc<Locale>,
+    language_tag: Rc<String>,
 }
 
 impl LString {
-    /// Creates a `LString` object from a reference to a string slice [`str`] and a reference counted [`Locale`].
+    /// Creates a `LString` from string slice `[str]` and a language tag as `&[Rc]<[String]>`.
+    /// 
+    /// *WARNING:* No checks are done on the supplied language tag to see if it conforms to the [BCP 47 Language Tag]
+    /// specification in terms of:
+    /// 
+    /// * _well-formed_ - syntactically correct,
+    /// 
+    /// * _valid_ - well-formed and only uses registered language subtags, extensions, keywords, types…,
+    /// 
+    /// * _canonical_ - valid and no deprecated codes or structure.
+    /// 
+    /// If required the `[Locale]::canonicalize()` function of `[icu_locid]` crate can be used to perform the
+    /// conformance checks.
     /// 
     /// # Examples
     /// 
@@ -92,20 +87,26 @@ impl LString {
     /// use i18n_lstring::LString;
     /// 
     /// let string = "This is a test string.";
-    /// let locale = Rc::new( "en-ZA".parse().expect( "Failed to parse language tag." ) );
-    /// let lang_string = LString::new( string, &locale );
+    /// let tag = Rc::new(
+    ///     Locale::canonicalize( "en-ZA" ).expect( "Failed to canonicalise language tag." )
+    /// );
+    /// let lang_string = LString::new( string, &tag );
     /// 
     /// assert_eq!( lang_string.as_str(), string, "String failed." );
-    /// assert_eq!( lang_string.locale(), &locale, "Locale failed." );
+    /// assert_eq!( lang_string.language_tag(), &tag, "Language tag failed." );
     /// ```
     /// 
-    /// [`str`]: https://doc.rust-lang.org/nightly/core/primitive.str.html
-    /// [`Locale`]: https://docs.rs/icu/latest/icu/locid/struct.Locale.html
-    pub fn new( string: &str, locale: &Rc<Locale> ) -> Self {
-        LString { string: String::from( string ), locale: Rc::clone( locale ) }
+    /// [str]: https://doc.rust-lang.org/core/primitive.str.html
+    /// [Rc]: https://doc.rust-lang.org/std/rc/struct.Rc.html
+    /// [String]: https://doc.rust-lang.org/std/string/struct.String.html
+    /// [BCP 47 Language Tag]: https://www.rfc-editor.org/rfc/bcp/bcp47.txt
+    /// [Locale]: https://docs.rs/icu/latest/icu/locid/struct.Locale.html
+    /// [icu_locid]: https://crates.io/crates/icu_locid
+    pub fn new<T: Into<String>>( string: T, language_tag: &Rc<String> ) -> Self {
+        LString { string: string.into(), language_tag: Rc::clone( language_tag ) }
     }
 
-    /// Returns a reference to the internal [`String`].
+    /// Returns a reference `&[str]` to the internal `[String]`.
     /// 
     /// # Examples
     /// 
@@ -115,18 +116,20 @@ impl LString {
     /// use i18n_lstring::LString;
     /// 
     /// let string = "This is a test string.";
-    /// let locale = Rc::new( "en-ZA".parse().expect( "Failed to parse language tag." ) );
-    /// let lang_string = LString::new( string, &locale );
+    /// let tag = Rc::new(
+    ///     Locale::canonicalize( "en-ZA" ).expect( "Failed to canonicalise language tag." )
+    /// );
+    /// let lang_string = LString::new( string, &tag );
     /// 
     /// assert_eq!( lang_string.as_str(), string, "String failed." );
     /// ```
-    /// 
-    /// [`String`]: https://doc.rust-lang.org/std/string/struct.String.html
+    /// [str]: https://doc.rust-lang.org/core/primitive.str.html
+    /// [String]: https://doc.rust-lang.org/std/string/struct.String.html
     pub fn as_str( &self ) -> &str {
         &self.string
     }
 
-    /// Returns a reference counted for [`Locale`].
+    /// Returns a reference to the language tag `[Rc]<[String]>`.
     /// 
     /// # Examples
     /// 
@@ -135,29 +138,35 @@ impl LString {
     /// use std::rc::Rc;
     /// use i18n_lstring::LString;
     /// 
-    /// let locale = Rc::new( "en-ZA".parse().expect( "Failed to parse language tag." ) );
-    /// let lang_string = LString::new( "This is a test string.", &locale );
+    /// let tag = Rc::new(
+    ///     Locale::canonicalize( "en-ZA" ).expect( "Failed to canonicalise language tag." )
+    /// );
+    /// let lang_string = LString::new( "This is a test string.", &tag );
     /// 
-    /// assert_eq!( lang_string.locale(), &locale, "Locale failed." );
+    /// assert_eq!( lang_string.language_tag(), &tag, "Locale failed." );
     /// ```
-    /// 
-    /// [`Locale`]: https://docs.rs/icu/latest/icu/locid/struct.Locale.html
-    pub fn locale( &self ) -> &Rc<Locale> {
-        &self.locale
+    /// [Rc]: https://doc.rust-lang.org/std/rc/struct.Rc.html
+    /// [String]: https://doc.rust-lang.org/std/string/struct.String.html
+    pub fn language_tag( &self ) -> &Rc<String> {
+        &self.language_tag
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use icu_locid::Locale;
+
     use super::*;
 
     #[test]
-    fn locale_and_string() {
+    fn string_with_language_tag() {
         let string = "This is a test string.";
-        let locale = Rc::new( "en-ZA".parse().expect( "Failed to parse language tag." ) );
-        let lang_string = LString::new( string, &locale );
+        let tag = Rc::new(
+            Locale::canonicalize( "en-ZA" ).expect( "Failed to canonicalise language tag." )
+        );
+        let lang_string = LString::new( string, &tag );
     
-        assert_eq!( lang_string.locale(), &locale, "Locale failed." );
+        assert_eq!( lang_string.language_tag(), &tag, "Language tag failed." );
         assert_eq!( lang_string.as_str(), string, "String failed." );
     }
 }
